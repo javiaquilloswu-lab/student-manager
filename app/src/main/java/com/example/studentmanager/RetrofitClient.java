8ac1a24f950557af5f52c5cf1116e2f2941a68bc
kingdavid28 <your.email@example.com>
2026-09-02 17:07:22 +0800

    hapit na gyud part 69

--- app/src/main/java/com/example/studentmanager/RetrofitClient.java
+++ app/src/main/java/com/example/studentmanager/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     // 10.0.2.2 maps to local machine localhost from Android Emulator
-    private static final String BASE_URL = "http://192.168.1.15/android_api/";
+    private static final String BASE_URL = "http://--/android_api/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
